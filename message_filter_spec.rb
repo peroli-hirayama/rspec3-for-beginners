c01ad6476b89d172a66ccc9917a12a223f19c463
Takayuki Hirayama <takayuki.hirayama@peroli.jp>
2015-04-14 14:25:56 +0900

    join lines

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -5,10 +5,6 @@ describe MessageFilter, 'with argument "foo"' do
     @filter = MessageFilter.new('foo')
   end
   subject { @filter }
-  it {
-    is_expected.to be_detect('hello from foo')
-  }
-  it {
-    is_expected.not_to be_detect('hello world!')
-  }
+  it { is_expected.to be_detect('hello from foo') }
+  it { is_expected.not_to be_detect('hello world!') }
 end
