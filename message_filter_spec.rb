61da5cd123a8a26d7c6c40719f3385b14a8a6045
Takayuki Hirayama <takayuki.hirayama@peroli.jp>
2015-04-16 12:54:08 +0900

    use predicate matcher

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -8,9 +8,8 @@ describe MessageFilter do
   context 'with argument "foo"' do
     subject { MessageFilter.new('foo') }
     it_behaves_like 'MessageFilter with argument "foo"'
-#    it { is_expected.not_to be_empty }
     it 'ng_words should not be empty' do
-      expect(subject.ng_words.empty?).to eq false
+      expect(subject.ng_words).not_to be_empty
     end
   end
   context 'with argument "foo","bar"' do
